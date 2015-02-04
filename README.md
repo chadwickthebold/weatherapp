1040d6873696e7c15c274f190733f3427f2c53f9
Tyler Chadwick <chadwickthebold@gmail.com>
2015-02-04 01:42:30 -0500

    wired up city search box and began adding styles

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This project is a demonstration of a simple app comparing the weather between two cities. It pulls data from the [OpenWeatherMap API](http://openweathermap.org/api).
 
+## Description
+
+As stated above, this project is a simple application of the OpenWeatherMap API. Two input boxes provide the ability to search for two cities to compare, defaulting to London and New York. The current weather metrics are shown below these inputs in tabular form by default. Two buttons in this section allow this table to toggle between current metrics and a 7-day forecast.
+
+The lower section displays an interactive chart which defaults to showing a forecast for the next 7-day period for temperature. Three buttons in this section allow toggling between temperature, atmosperic conditions (cloud cover and snow), and wind. The chart combines information from both cities. A control is available under the chart to control the time range for this information. Time permitting, this will be expanded to allow viewing of historical weather data from the past month. 
+
+Several options for configuration are available under the gear icon in the header. These options include unit conversion, layout, and themeing. 
+
 ## Installation
 
 1. Pull this repo
@@ -9,7 +17,22 @@ This project is a demonstration of a simple app comparing the weather between tw
 	* Optionally change the API key in js/main.js to your own API key
 3. Run ```gulp serve``` to start the app on localhost:4444
 
+A deployed version of this project can be viewed at [tylerchadwick.com/weatherapp](http://tylerchadwick.com/weatherapp)
+
+## Process
+
+1. Requirements
+2. Research
+3. Design
+4. Iteration
+5. Delivery
+
+
 ## TODO
 
 - [ ] Add unrequired node_modules and bower_components directories to .gitignore
 - [ ] Create a distribution build with concat'd/min'd/uglify'd files
+- [ ] Create an interactive chart using d3, c3, or the google charting API
+- [ ] Adjust the tooltip styles to reflect the selected theme
+- [ ] Verify Browser support
+- [ ] Use promises in a more generic way in resolving lat/long coordinates and states
--- js/main.js
+++ js/main.js
@@ -1,7 +1,262 @@
 jQuery(function() {
 
+
 // Application namespace
 var weatherapp = weatherapp || {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Utility functions
+
+// Debounce function from underscore
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+
+
+
+
+
+
+
+
+/**********************************
+ Application Configuration
+**********************************/
+
+
+
+// Configuration options
+weatherapp.options = {
+	'lang' : 'en',
+	'units' : 'metric'
+};
+
+
+
+
+
+// OpenWeatherMap specific configuration
+weatherapp.openweathermap = {}
+
+weatherapp.openweathermap.apikey = 'b3bca1dfe94a0bce42d25c1533403005';
+
+weatherapp.openweathermap.urls = {
+	'find' : 'http://api.openweathermap.org/data/2.5/find',
+	'current' : 'http://api.openweathermap.org/data/2.5/weather',
+	'forecast3h' : 'http://api.openweathermap.org/data/2.5/forecast',
+	'forecast16d' : 'http://api.openweathermap.org/data/2.5/forecast/daily',
+	'history' : 'http://api.openweathermap.org/data/2.5/history/city'
+};
+
+weatherapp.openweathermap.options = {};
+
+weatherapp.openweathermap.options.find =  {
+	'q' : '',
+	'type' : 'like',
+	'units' : weatherapp.options.units,
+	'APIID' : weatherapp.openweathermap.apikey
+};
+
+
+
+
+
+
+
+
+// reverse geocoding specific configuration
+weatherapp.geocoding = {
+	'apikey' : 'AIzaSyC6y5K_nqcuCxnqOTpKc6JVA2M0p_vQ9oI',
+	'url' : 'https://maps.googleapis.com/maps/api/geocode/json'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************
+ City Search
+**********************************/
+
+
+
+
+
+
+
+
+
+
+function populateSearchSuggestions(input, data) {
+	var searchbox = $(input),
+			container = searchbox.closest('.wa-city-search'),
+			suggestionList = container.find('.wa-search-suggestions').empty(),
+			icons = $(input).siblings('.wa-icon-container'),
+			geocodingParams,
+			listItem,
+			suggestionElem;
+
+	if (data.count) {
+
+		for (var i = 0; i < data.count; i++) {
+			listItem = data.list[i];
+			suggestionElem = $('<li class="wa-suggestions-elem">');
+
+			if (listItem.name) {
+				suggestionElem.append(listItem.name)
+			}
+
+			if (listItem.sys && listItem.sys.country) {
+				if (listItem.sys.country == 'US') {
+					// icons.find('.fa-spinner').addClass('is-active').removeClass('is-inactive');
+
+					// geocodingParams = {
+					// 	'key' : weatherapp.geocoding.apikey,
+					// 	'latlng' : listItem.coord.lat + ',' + listItem.coord.lon
+					// }
+
+					// $.get(weatherapp.geocoding.url + '?' + $.param(geocodingParams))
+					// 	.done(function(data) {
+					// 		//suggestionElem.append(', ' + data.results[0].addressComponents);
+					// 	}).fail(function() {
+
+					// 	}).always(function() {
+					// 		icons.find('.fa-spinner').addClass('is-inactive').removeClass('is-active');
+					// 		suggestionElem.append(', ' + listItem.sys.country);
+					// 	});
+
+					
+					
+
+
+				} else {
+					suggestionElem.append(', ' + listItem.sys.country);
+				}
+			}
+
+			suggestionList.append(suggestionElem);
+		}
+
+		suggestionList.css('min-width', container.width());
+		suggestionList.toggleClass('is-closed');
+
+	} else {
+		icons.find('.fa-exclamation-circle').toggleClass('is-active is-inactive');
+	}
+}
+
+
+
+
+
+
+
+
+function citysearch() {
+	var input = this,
+			findParameters = $.extend({}, weatherapp.openweathermap.options.find, {'q':input.value}),
+			urlString = weatherapp.openweathermap.urls.find + '?' + $.param(findParameters),
+			icons = $(input).siblings('.wa-icon-container');
+
+	icons.find('.fa-spinner').toggleClass('is-active is-inactive');
+	console.log('making ajax request for q=' + input.value);
+
+
+	$.get(urlString)
+		.done(function(data) {
+			icons.find('.fa-spinner').toggleClass('is-active is-inactive');
+			populateSearchSuggestions(input, data);
+		}).fail(function() {
+			icons.find('.fa-spinner').toggleClass('is-active is-inactive');
+			icons.find('.fa-exclamation-triangle').toggleClass('is-active is-inactive');
+		});
+};
+
+var citysearch_debounced = debounce(citysearch, 750);
+
+
+
+
+
+
+
+
+
+
+// Search funtionality
+$('#city1-search').on('keydown.city1-search', citysearch_debounced);
+
+
+
+// Tooltip functionality
+$('.fa[title]').qtip({
+	style : {
+		classes : 'wa-tooltip qtip-shadow'
+	},
+	position: {
+		my: 'bottom center',
+		at: 'top center'
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+/**********************************
+ Forecast Charting
+**********************************/
+
+
+
+
+// Should end up with these elements
+weatherapp.city1 = {}
+weatherapp.city2 = {}
+weatherapp.chart = {}
+
+
+
 
 
 
